a97fa603620215c4d539e8313a55933f092eedfb
josevadri <josevayac@gmail.com>
2016-05-17 02:47:37 -0600

    Creacion proyecto

---
+++ Clientes/Migrations/201605170844378_Carlos.cs
@@ -0,0 +1,16 @@
+namespace Clientes.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Carlos : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Clientes/Models/persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clientes.Models
+{
+    public class persona
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string lastname { get; set; }
+
+        [Required]
+        public int edad { get; set; }
+    }
+}
